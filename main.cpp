cfd8852dfe076003d28510d3d185535910653658
Uwami Mgxekwa <112516594+Uwami-Mgxekwa@users.noreply.github.com>
2024-02-18 19:48:11 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,407 @@
+#include <iostream>
+
+using namespace std;
+
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  char ch[500];
+ printf("----------------------------------------\n\n");
+ printf(" inter your name :: \n");
+
+ printf("----------------------------------------\n");
+
+ gets(ch);
+for(int i=0;ch[i] != '\0';i++){
+ switch(ch[i])
+{
+case 'a':
+case 'A':
+
+                printf("\n ......a........ ");
+                printf("\n......a..a.......");
+                printf("\n.....a....a......");
+                printf("\n....aaaaaaaa.....");
+                printf("\n...a........a....");
+                printf("\n..a..........a...");
+
+ break;
+case 'b':
+case 'B':
+                printf("\n");
+                printf("\n         bbbbbb");
+                printf("\n         b     bb");
+                printf("\n         b.    bb");
+                printf("\n         bbbbbbbb");
+                printf("\n         b     bb");
+                printf("\n         b     bb");
+                printf("\n         b.    bb");
+                printf("\n         bbbbbbb");
+ break;
+
+case 'c':
+case 'C':
+                printf("\n");
+                printf("\n       .cccccc    ");
+                printf("\n       cc        ");
+                printf("\n       cc        ");
+                printf("\n       cc        ");
+                printf("\n       cc        ");
+                printf("\n       .cccccc    ");
+
+
+break;
+
+
+case 'd':
+case 'D':
+                printf("\n");
+                printf("\n          dddddddd    ");
+                printf("\n          d       d   ");
+                printf("\n          d        d  ");
+                printf("\n          d         d ");
+                printf("\n          d         d ");
+                printf("\n          d        d  ");
+                printf("\n          ddddddddd   ");
+
+break;
+
+case 'e':
+case 'E':
+
+                printf("\n");
+                printf("\n          eeeeeeeee     ");
+                printf("\n          ee            ");
+                printf("\n          ee            ");
+                printf("\n          eeeeeee       ");
+                printf("\n          ee            ");
+                printf("\n          ee            ");
+                printf("\n          eeeeeeeee     ");
+
+
+break;
+
+case 'f':
+case 'F':
+
+
+                printf("\n");
+                printf("\n          ffffffffff ");
+                printf("\n          ff         ");
+                printf("\n          ff         ");
+                printf("\n          fffffff    ");
+                printf("\n          ff         ");
+                printf("\n          ff         ");
+                printf("\n          ff         ");
+
+break;
+
+case 'g':
+case 'G':
+
+
+
+                printf("\n");
+                printf("\n         ggggggg    ");
+                printf("\n        gg          ");
+                printf("\n        gg          ");
+                printf("\n        gg   ggggggg");
+                printf("\n        gg        gg" );
+                printf("\n        gg        gg");
+                printf("\n        gggggggggggg");
+
+break;
+
+case 'h':
+case 'H':
+                printf("\n");
+                printf("\n          hh       hh   ");
+                printf("\n          hh       hh   ");
+                printf("\n          hh       hh   ");
+                printf("\n          hhhhhhhhhhh   ");
+                printf("\n          hh       hh   ");
+                printf("\n          hh       hh   ");
+                printf("\n          hh       hh   ");
+
+
+break;
+case 'i':
+case 'I':
+                printf("\n");
+                printf("\n \t        iiii        ");
+                printf("\n \t         ii         ");
+                printf("\n \t         ii         ");
+                printf("\n \t         ii         ");
+                printf("\n \t         ii         ");
+                printf("\n \t         ii         ");
+                printf("\n \t        iiii        ");
+
+break;
+
+case 'j':
+case 'J':
+
+               printf("\n");
+                printf("\n \t         jj         ");
+                printf("\n \t         jj         ");
+                printf("\n \t         jj         ");
+                printf("\n \t         jj         ");
+                printf("\n \t  j      jj         ");
+                printf("\n \t  j      jj         ");
+                printf("\n \t   jjjjjj           ");
+
+break;
+
+
+case 'k':
+case 'K':
+                printf("\n");
+                printf("\n \t         kk          ");
+                printf("\n \t         kk   kk     ");
+                printf("\n \t         kk kk       ");
+                printf("\n \t         kkk         ");
+                printf("\n \t         kk kk       ");
+                printf("\n \t         kk   kk     ");
+                printf("\n \t         kk          ");
+
+break;
+
+
+case 'l':
+case 'L':
+                printf("\n");
+                printf("\n \t         ll          ");
+                printf("\n \t         ll          ");
+                printf("\n \t         ll          ");
+                printf("\n \t         ll          ");
+                printf("\n \t         ll          ");
+                printf("\n \t         ll          ");
+                printf("\n \t         llllllllll  ");
+
+break;
+
+case 'm':
+case 'M':
+                printf("\n");
+                printf("\n \t         mm           mm  ");
+                printf("\n \t         mm m       m mm  ");
+                printf("\n \t         mm  m     m  mm  ");
+                printf("\n \t         mm   m   m   mm  ");
+                printf("\n \t         mm    m m    mm  ");
+                printf("\n \t         mm     m     mm  ");
+                printf("\n \t         mm           mm  ");
+
+
+break;
+
+case 'n':
+case 'N':
+                printf("\n");
+                printf("\n \t         nn       nn   ");
+                printf("\n \t         nn n     nn   ");
+                printf("\n \t         nn  n    nn   ");
+                printf("\n \t         nn   n   nn   ");
+                printf("\n \t         nn    n  nn   ");
+                printf("\n \t         nn     n nn   ");
+                printf("\n \t         nn       nn   ");
+
+break;
+
+
+
+case 'o':
+case 'O':
+
+                printf("\n");
+                printf("\n \t          ooooo      ");
+                printf("\n \t        oo     oo    ");
+                printf("\n \t       oo       oo   ");
+                printf("\n \t      oo         oo  ");
+                printf("\n \t      oo         oo  ");
+                printf("\n \t       oo       oo   ");
+                printf("\n \t          ooooo      ");
+
+break;
+
+case 'p':
+case 'P':
+
+                printf("\n");
+                printf("\n \t         pppppppp      ");
+                printf("\n \t         pp     pp     ");
+                printf("\n \t         pp     pp     ");
+                printf("\n \t         pppppppp      ");
+                printf("\n \t         pp            ");
+                printf("\n \t         pp            ");
+                printf("\n \t         pp            ");
+
+break;
+
+
+case 'q':
+case 'Q':
+
+                printf("\n");
+                printf("\n \t          qqqqq      ");
+                printf("\n \t        qw     qq    ");
+                printf("\n \t       qq q     qq   ");
+                printf("\n \t      qw    q   qq   ");
+                printf("\n \t      qq     q   qq  ");
+                printf("\n \t       qq     q qq   ");
+                printf("\n \t          qqqqq     ");
+
+break;
+
+case 'r':
+case 'R':
+
+
+                printf("\n");
+                printf("\n \t         rrrrrrrr      ");
+                printf("\n \t         rr     rr     ");
+                printf("\n \t         rr     rr     ");
+                printf("\n \t         rrrrrrrr      ");
+                printf("\n \t         rr rr         ");
+                printf("\n \t         rr  rr        ");
+                printf("\n \t         rr   rr       ");
+
+break;
+
+case 's':
+case 'S':
+
+                printf("\n");
+                printf("\n \t         ssssssss    ");
+                printf("\n \t         ss          ");
+                printf("\n \t         ss          ");
+                printf("\n \t         ssssssss    ");
+                printf("\n \t               ss    ");
+                printf("\n \t               ss    ");
+                printf("\n \t         ssssssss    ");
+
+break;
+
+case 't':
+case 'T':
+
+                printf("\n");
+                printf("\n \t      ttttttttt     ");
+                printf("\n \t         tt         ");
+                printf("\n \t         tt         ");
+                printf("\n \t         tt         ");
+                printf("\n \t         tt         ");
+                printf("\n \t         tt         ");
+                printf("\n \t         tt         ");
+
+break;
+
+case 'u':
+case 'U':
+
+                printf("\n");
+                printf("\n \t         uu       uu     ");
+                printf("\n \t         uu       uu     ");
+                printf("\n \t         uu       uu     ");
+                printf("\n \t         uu       uu     ");
+                printf("\n \t         uu       uu     ");
+                printf("\n \t         uu       uu     ");
+                printf("\n \t         uuuuuuuuuuu     ");
+
+
+break;
+
+case 'v':
+case 'V':
+
+                printf("\n");
+                printf("\n \t         vv       vv    ");
+                printf("\n \t         vv       vv    ");
+                printf("\n \t          vv     vv     ");
+                printf("\n \t          vv     vv     ");
+                printf("\n \t          vv     vv     ");
+                printf("\n \t            vv  vv      ");
+                printf("\n \t              vv        ");
+
+
+break;
+
+case 'w':
+case 'W':
+                printf("\n");
+                printf("\n \t         ww    w    ww     ");
+                printf("\n \t         ww    w    ww     ");
+                printf("\n \t         ww    w    ww     ");
+                printf("\n \t         ww    w    ww     ");
+                printf("\n \t         ww    w    ww     ");
+                printf("\n \t         ww    w    ww     ");
+                printf("\n \t          wwwwwwwwwww      ");
+
+break;
+
+
+case 'x':
+case 'X':
+
+                printf("\n");
+                printf("\n \t         xx       xx   ");
+                printf("\n \t          xx     xx    ");
+                printf("\n \t           xx   xx     ");
+                printf("\n \t            xxxx       ");
+                printf("\n \t             xx        ");
+                printf("\n \t          xx   xx      ");
+                printf("\n \t        xx      xx.    ");
+
+break;
+
+case 'y':
+case 'Y':
+
+
+
+                printf("\n");
+                printf("\n \t         yy       yy    ");
+                printf("\n \t         yy       yy    ");
+                printf("\n \t          yy     yy     ");
+                printf("\n \t           yy   yy      ");
+                printf("\n \t              yy        ");
+                printf("\n \t              yy        ");
+                printf("\n \t              yy        ");
+
+
+break;
+
+case 'z':
+case 'Z':
+
+                printf("\n");
+                printf("\n \t     zzzzzzzz        ");
+                printf("\n \t           zz        ");
+                printf("\n \t          zz         ");
+                printf("\n \t         zz          ");
+                printf("\n \t        zz           ");
+                printf("\n \t       zz            ");
+                printf("\n \t      zzzzzzzz       ");
+
+break;
+
+
+
+
+
+
+
+
+
+default:
+ printf("⚠️ ⚠️ warning ⚠️⚠️ \a");
+
+}
+}
+printf("\n\n\n");
+printf("\t \t Thank you ❣️❣️");
+  return 0;
+}
+
